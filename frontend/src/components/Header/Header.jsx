79969e95a4679df5d174184ead07e287e7a74a73
sayantan1413 <sayantanbose2001@gmail.com>
2024-09-15 16:27:21 +0530

    Added the header component content and design

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Order your favourite food here</h2>
+            <p>Discover a Diverse Menu of Delicious Dishes, Fresh Ingredients, and Irresistible Flavors – Ready to Order for Fast, Convenient Delivery Right to Your Doorstep. Satisfy Your Cravings Today!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
